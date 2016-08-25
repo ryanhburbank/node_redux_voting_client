8214608be0bd5111eebf11f4b080b63efc21e5b4
Ryan Burbank <rburbank@streamsend.com>
2016-08-24 23:17:35 -0700

    Add react-router and initial Routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 const pair = ['Chinatown', 'Children of Men'];
 
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-  <Voting pair={pair}/>,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 );
 
