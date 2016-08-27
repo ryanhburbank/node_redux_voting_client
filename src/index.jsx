5593873dd21f277ff0025f9c3c296516f9082f81
Ryan Burbank <rburbank@streamsend.com>
2016-08-27 16:22:48 -0700

    Add Redux store, connect store with Voting component

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux';
 import App from './components/App';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
 import Results from './components/Results';
 
-const pair = ['Chinatown', 'Children of Men'];
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Chinatown', 'Snatch'],
+      tally: {Chinatown: 2}
+    }
+  }
+});
 
 const routes = <Route component={App}>
   <Route path="/results" component={Results} />
-  <Route path="/" component={Voting} />
+  <Route path="/" component={VotingContainer} />
 </Route>;
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
 
