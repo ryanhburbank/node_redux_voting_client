88c5e52ef97e19c00ef49cf2aeec1aa0ac4c086e
Ryan Burbank <rburbank@streamsend.com>
2016-08-28 10:22:18 -0700

    Add socket.io-client, connect to Socket.io server, add event listeners

--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
-import reducer from './reducer';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Chinatown', 'Snatch'],
-      tally: {Chinatown: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+ store.dispatch({type: 'SET_STATE', state})
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
