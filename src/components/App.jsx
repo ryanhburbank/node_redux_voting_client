fdf79522b6702964ecf4a234034bcb526fa9b541
Ryan Burbank <rburbank@streamsend.com>
2016-08-27 22:25:03 -0700

    Implement ResultsContainer to connect store to Results component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const pair = List.of('Chinatown', 'Snatch');
-const tally = Map({'Chinatown': 3, 'Snatch': 1});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      tally: tally
-    });
+    return this.props.children;
   }
 });
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import reducer from './reducer';
 import {Provider} from 'react-redux';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
-import Results from './components/Results';
+import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
 store.dispatch({
@@ -20,7 +20,7 @@ store.dispatch({
 });
 
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
+  <Route path="/results" component={ResultsContainer} />
   <Route path="/" component={VotingContainer} />
 </Route>;
 
